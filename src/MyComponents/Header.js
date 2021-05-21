6ab989e6cc4590c6ce996f01ec80abd10f4b5b65
Apurva1312 <61040914+Apurva1312@users.noreply.github.com>
2021-05-21 21:42:11 +0530

    Update Header.js

--- src/MyComponents/Header.js
+++ src/MyComponents/Header.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Carousel } from 'react-bootstrap';
 import './Header.css'
-
-
-function Header() {
+import { Button , Col, Form } from 'react-bootstrap';
+import {IndianStates} from '../utils/dataHelpers';
+import './MainPage.css'
+ function Header() {
   return (
-  <div className="header">
-    <div ClassName="carousel-effect">
+    <div ClassName="header">
+        <div ClassName="carousel-effect">
   <Carousel>
   <Carousel.Item>
     <img
@@ -32,8 +33,40 @@ function Header() {
     />
   </Carousel.Item>
 </Carousel>
+
+<div className = "container">
+       <h2>Check Vaccine Availability</h2>
+      <Form>
+      <Form.Row>
+      <Form.Group as={Col} controlId="formGridCity">
+      <Form.Label>City</Form.Label>
+      <Form.Control />
+      </Form.Group>
+      <Form.Group as={Col} controlId="formGridState">
+      <Form.Label>State</Form.Label>
+      <Form.Control as="select" defaultValue="Choose...">
+      <option>Choose...</option>
+      {
+          IndianStates.map(state=>{
+            return <Form.Control as="option" value={state}>{state}</Form.Control>
+        })
+    }
+    </Form.Control>
+    </Form.Group>
+    <Form.Group as={Col} controlId="formGridZip">
+    <Form.Label>Zip</Form.Label>
+    <Form.Control />
+    </Form.Group>
+    </Form.Row>
+    <Button variant="primary" type="submit">
+    Search
+    </Button>
+    </Form> 
+    </div>  
+    
+
 </div>
-    </div>
+</div>
   )
 }
-export default Header
+export default Header 
