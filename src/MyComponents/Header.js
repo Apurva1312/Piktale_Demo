d775485ccac27f7a8539d0f00b1a5b23c9e8b434
prats1208 <72569885+prats1208@users.noreply.github.com>
2021-05-24 00:32:09 +0530

    Update Header.js

--- src/MyComponents/Header.js
+++ src/MyComponents/Header.js
@@ -1,29 +1,52 @@
+
 import React, { useEffect, useState } from 'react'
 import { Carousel } from 'react-bootstrap';
 import './Header.css'
 import { Button , Col, Form } from 'react-bootstrap';
-import {IndianStates} from '../utils/dataHelpers';
 import './MainPage.css'
-import axios from 'axios'
-
+import axios from 'axios';
  function Header() {
   const[states,setStates] = useState([])
-  // const[users,setUsers]=useState([])
+  const[districts,setDistricts]=useState([])
   const[text,setText] = useState('')
-  // const[suggestions,setSuggestions] = useState([])
+  const[suggestions,setSuggestions] = useState([])
+  const[selectstate,setSelectstate] = useState()
+  const[x,setX]= useState('')
+  const[options,setOptions] = useState([])
+  
   useEffect(() => {
     const loadstates = async() =>{
-      // https://reqres.in/api/users
-      // https://cdn-api.co-vin.in/api/v2/admin/location/states
-      const response = await axios.get('https://cdn-api.co-vin.in/api/v2/admin/location/state')
+      const response = await axios.get('https://cdn-api.co-vin.in/api/v2/admin/location/states')
       console.log(response.data)
       setStates(response.data.states)
-      
     }
     loadstates();
 
   },[])
 
+  const onSuggestHandler =(text)=>{
+    // response.data.states.filter(state=>state_name == text).map(state=>state_id)
+    setText(text)
+    setSuggestions([])
+    // setSelectstate(res.filter(state=>state_name == text).map(state=>state_id))
+  }
+
+  useEffect(() => {
+    const loadcities = async() =>{
+      const response = await axios.get(`https://cdn-api.co-vin.in/api/v2/admin/location/districts/{selectstate}`)
+      console.log(response.data.districts)
+      setDistricts(response.data.districts)
+      
+    }
+    loadcities();
+
+  },[])
+  
+  const onOptionHandler =(x)=>{
+    setX(x)
+    setOptions([])
+  }
+  
   const onChangeHandler = (text) => {
     let matches = []
     if(text.length > 0){
@@ -33,11 +56,25 @@ import axios from 'axios'
       })
     }
       console.log('matches',matches)
-      // setSuggestions(matches)
+      setSuggestions(matches)
       setText(text)
-
+      
   }
 
+  const onchangeHandler = (x) => {
+    let matches = []
+    if(x.length > 0){
+      matches = districts.filter(district =>{
+        const regex = new RegExp(`${x}`,"gi")
+        return district.district_name.match(regex)
+      })
+    }
+      console.log('matches',matches)
+      setOptions(matches)
+      setX(x)
+
+  }
+ 
 
   return (
     <div className="header">
@@ -74,14 +111,31 @@ import axios from 'axios'
       <Form.Row>
       <Form.Group as={Col} controlId="formGridState">
       <Form.Label>State</Form.Label>
-      <input id="formGridState" className="form-control state" type="text" onChange={e => onChangeHandler(e.target.value)} value={text}/>
-      {/* {suggestions && suggestions.map((suggestion,i)=>
-        //<div key={i}>{suggestion.state_name}</div>
-      //)} */}
+      <input id="formGridState" className="form-control state" type="text"
+       onChange={e => onChangeHandler(e.target.value)} value={text} 
+       onBlur ={()=>{
+        setTimeout(()=>{
+        setSuggestions([])
+         },300)
+       }} 
+      
+       />
+      {suggestions && suggestions.map((suggestion,i)=>
+        <div key={i} className="suggestion" onClick={() => 
+        onSuggestHandler(suggestion.state_name)} 
+        
+        >{suggestion.state_name}</div>
+      )}
       </Form.Group>
       <Form.Group as={Col} controlId="formGridCity">
       <Form.Label>City</Form.Label>
-      <input id="formGridCity" className="form-control city" type="text"></input>
+      <input id="formGridCity" className="form-control city" type="text"
+       onChange={e => onchangeHandler(e.target.value)} value={x}
+      />
+      {options && options.map((option,i)=>
+        <div key={i} className="options" onClick={() => onOptionHandler(option.district_name)}
+        >{option.district_name}</div>
+      )}
       </Form.Group>
       <Form.Group as={Col} controlId="formGridZip">
       <Form.Label>Zip</Form.Label>
@@ -100,4 +154,3 @@ import axios from 'axios'
   )
 }
 export default Header 
-
